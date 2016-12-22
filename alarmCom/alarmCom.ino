9b3aa9c490917fb0681ee3145add9ca12df8d078
pfory <pfory@seznam.cz>
2016-12-22 10:04:18 +0100

    +mqtt

--- alarmCom/alarmCom.ino
+++ alarmCom/alarmCom.ino
@@ -1,13 +1,15 @@
 /**************************************************************
  *
- * This script tries to auto-detect the baud rate
- * and allows direct AT commands access
- *
- * TinyGSM Getting Started guide:
- *   http://tiny.cc/tiny-gsm-readme
  *
  **************************************************************/
-
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+#include <TinyGsmClient.h>
+#include <Ethernet.h>
+#include <EthernetClient.h>
+#include <Dns.h>
+#include <Dhcp.h>
+ 
 // Set serial for debug console (to the Serial Monitor, speed 115200)
 #define SerialMon Serial
 
@@ -15,6 +17,7 @@
 // Use Hardware Serial on Mega, Leonardo, Micro
 #define SerialAT Serial1
 
+#define LOOPALARM LOW
 #define LOOP0 9
 #define LOOP1 10
 #define LOOP2 11
@@ -25,24 +28,72 @@ bool isAlarmCall = false;
 bool isArmed = true;
 unsigned long alarmCallStart;
 unsigned long alarmCallDelay=20000;
+uint32_t loopStatus = 0;
 
-#include <TinyGsmClient.h>
+byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xCC};
+
+#define AIO_SERVER      "178.77.238.20"
+#define AIO_SERVERPORT  1883
+#define AIO_USERNAME    "datel"
+#define AIO_KEY         "hanka12"
+
+EthernetClient client;
+
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
+unsigned int const sendTimeDelay=30000;
+signed long lastSendTime = sendTimeDelay * -1;
+
+Adafruit_MQTT_Publish _loopStatus = Adafruit_MQTT_Publish(&mqtt,  "/home/bedNew/alarm/loopStatus");
+
 TinyGsm modem(SerialAT);
 
-#define PORT_SPEED 115200
+#define SERIALSPEED 115200
 
 void setup() {
   // Set console baud rate
-  SerialMon.begin(PORT_SPEED);
+  SerialMon.begin(SERIALSPEED);
   SerialMon.println("Alarm");
   pinMode(LOOP0, INPUT_PULLUP);
   delay(5000);
   modemSetup();
+
+  if (Ethernet.begin(mac) == 0) {
+    SerialMon.println("Failed using DHCP");
+  }
+  SerialMon.print("\nIP:");
+  SerialMon.println(Ethernet.localIP());
+  SerialMon.print("Mask:");
+  SerialMon.println(Ethernet.subnetMask());
+  SerialMon.print("Gateway:");
+  SerialMon.println(Ethernet.gatewayIP());
+  SerialMon.print("DNS:");
+  SerialMon.println(Ethernet.dnsServerIP());
+  SerialMon.println();
 }
 
 
 void loop() {
-  if (digitalRead(LOOP0)==LOW) {
+  if (millis() - lastSendTime >= sendTimeDelay) {
+    MQTT_connect();
+    lastSendTime = millis();
+
+    loopStatus = 0;
+    loopStatus = digitalRead(LOOP0);
+    loopStatus = 1 << digitalRead(LOOP1) | loopStatus;
+    loopStatus = 2 << digitalRead(LOOP2) | loopStatus;
+    loopStatus = 3 << digitalRead(LOOP3) | loopStatus;
+    loopStatus = 4 << digitalRead(LOOP4) | loopStatus;
+      
+    SerialMon.println(loopStatus);
+    
+    if (! _loopStatus.publish(loopStatus)) {
+      SerialMon.println(F("Failed"));
+    } else {
+      SerialMon.println(F("OK!"));
+    }
+  }
+  
+  if (digitalRead(LOOP0)==LOOPALARM) {
     if (!isAlarmCall && isArmed) {
       isAlarmCall=true;
       alarmCallStart = millis();
@@ -57,9 +108,11 @@ void loop() {
       SerialMon.println("Incoming call..");
       if (incBytes.indexOf("724232639")>0) {
         SerialMon.println("Identify number as known..");
-        SerialAT.write("ATA\n");
+        sendCommand("ATA");
+//        SerialAT.write("ATA\n");
       } else {
-        SerialAT.write("ATH\n");
+        sendCommand("ATH");
+        // SerialAT.write("ATH\n");
       }
     }
   }
@@ -67,19 +120,40 @@ void loop() {
   if (isAlarmCall) {
     if (millis() - alarmCallStart >= alarmCallDelay) {
         SerialMon.println("Alarm, hang up line...");
-        SerialAT.write("ATH\n");
+        sendCommand("ATH");
+        // SerialAT.write("ATH\n");
         isAlarmCall=false;
     }
   }
   
-  
-  
-  
   if (SerialMon.available()) {
     SerialAT.write(SerialMon.read());
   }
-  //delay(100);
-  //SerialMon.println(millis());
+  
+  if(! mqtt.ping()) {
+    mqtt.disconnect();
+  }
+}
+
+// Function to connect and reconnect as necessary to the MQTT server.
+// Should be called in the loop function and it will take care if connecting.
+void MQTT_connect() {
+  int8_t ret;
+
+  // Stop if already connected.
+  if (mqtt.connected()) {
+    return;
+  }
+
+  SerialMon.print("Connecting to MQTT... ");
+
+  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+       Serial.println(mqtt.connectErrorString(ret));
+       Serial.println("Retrying MQTT connection in 5 seconds...");
+       mqtt.disconnect();
+       delay(5000);  // wait 5 seconds
+  }
+  SerialMon.println("MQTT Connected!");
 }
 
 void modemSetup() {
@@ -118,37 +192,53 @@ void modemSetup() {
   SerialMon.println(F(" If it doesn't work, select \"Both NL & CR\" in Serial Monitor"));
   SerialMon.println(F("***********************************************************"));
   
-  SerialAT.write("AT+CMGF=1\n");
-  delay(3000);
-  if (SerialAT.available()) {
-    String incBytes = SerialAT.readStringUntil("\n");
-    SerialMon.println(incBytes);
-  }
-  SerialAT.write("AT+CNMI=0,0\n");
-  delay(3000);
-  if (SerialAT.available()) {
-    String incBytes = SerialAT.readStringUntil("\n");
-    SerialMon.println(incBytes);
-  }
-  SerialAT.write("AT+CREG?\n");
-  delay(3000);
-  if (SerialAT.available()) {
-    String incBytes = SerialAT.readStringUntil("\n");
-    SerialMon.println(incBytes);
-  }
-  SerialAT.write("AT+CSQ\n");
-  delay(3000);
-  if (SerialAT.available()) {
-    String incBytes = SerialAT.readStringUntil("\n");
-    SerialMon.println(incBytes);
-  }
-  SerialAT.write("AT+COPS?\n");
-  delay(3000);
-  if (SerialAT.available()) {
-    String incBytes = SerialAT.readStringUntil("\n");
-    SerialMon.println(incBytes);
-  }
-  SerialAT.write("AT+CLIP=1\n");
+  sendCommand("AT+CMGF=1");
+  // SerialAT.write("AT+CMGF=1\n");
+  // delay(3000);
+  // if (SerialAT.available()) {
+    // String incBytes = SerialAT.readStringUntil("\n");
+    // SerialMon.println(incBytes);
+  // }
+  sendCommand("AT+CNMI=0,0");
+  // SerialAT.write("AT+CNMI=0,0\n");
+  // delay(3000);
+  // if (SerialAT.available()) {
+    // String incBytes = SerialAT.readStringUntil("\n");
+    // SerialMon.println(incBytes);
+  // }
+  sendCommand("AT+CREG?");
+  // SerialAT.write("AT+CREG?\n");
+  // delay(3000);
+  // if (SerialAT.available()) {
+    // String incBytes = SerialAT.readStringUntil("\n");
+    // SerialMon.println(incBytes);
+  // }
+  sendCommand("AT+CSQ");
+  // SerialAT.write("AT+CSQ\n");
+  // delay(3000);
+  // if (SerialAT.available()) {
+    // String incBytes = SerialAT.readStringUntil("\n");
+    // SerialMon.println(incBytes);
+  // }
+  sendCommand("AT+COPS?");
+  // SerialAT.write("AT+COPS?\n");
+  // delay(3000);
+  // if (SerialAT.available()) {
+    // String incBytes = SerialAT.readStringUntil("\n");
+    // SerialMon.println(incBytes);
+  // }
+  sendCommand("AT+CLIP=1");
+  // SerialAT.write("AT+CLIP=1\n");
+  // delay(3000);
+  // if (SerialAT.available()) {
+    // String incBytes = SerialAT.readStringUntil("\n");
+    // SerialMon.println(incBytes);
+  // }
+}
+
+void sendCommand(String command) {
+  command += "\n";
+  SerialAT.println(command);
   delay(3000);
   if (SerialAT.available()) {
     String incBytes = SerialAT.readStringUntil("\n");
